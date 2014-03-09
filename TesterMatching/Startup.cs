16f7c03e0fda322b228401a5b27bd8f2e93d8a57
ruhveen <guy_mek@hotmail.com>
2014-03-09 20:11:52 +0200

    MatchingUTEST

---
+++ TesterMatching/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TesterMatching.Startup))]
+namespace TesterMatching
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
